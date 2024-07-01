b04a6441b705f6432ad8319b55085f93b24bdf89
MikaelM <michaelvalyan@gmail.com>
2024-06-30 20:03:35 -0700

    destroy

--- EKS-module/eks.tf
+++ EKS-module/eks.tf
@@ -2,19 +2,26 @@
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "${var.project_name}-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
-  # version  = var.cluster_version
-  version  = "1.28"
+  version  = var.cluster_version
 
 
   vpc_config {
-    endpoint_private_access = false
+    endpoint_private_access = true
     endpoint_public_access  = true
-    subnet_ids              = var.subnet_ids #public
+    public_access_cidrs     = ["0.0.0.0/0"] # Restrict to specific CIDR ranges
+    subnet_ids              = var.subnet_ids #private
     security_group_ids      = [aws_security_group.eks_sg.id]
   }
 
   kubernetes_network_config {
     service_ipv4_cidr = var.k8s_service_cidr
+  }
+
+  encryption_config {
+    resources = ["secrets"]
+    provider {
+      key_arn = aws_kms_key.eks_kms_key.arn
+    }
   }
 
   depends_on = [
