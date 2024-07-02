6b96ffa1ebb425c5d704aff4ae55d3b9481a8245
MikaelM <michaelvalyan@gmail.com>
2024-07-01 17:47:16 -0700

    craete an old version

--- roots/devops-project-main/providers.tf
+++ roots/devops-project-main/providers.tf
@@ -4,11 +4,18 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = module.eks-cluster.cluster_endpoint
-  token                  = module.eks-cluster.eks_cluster_token
+  # token                  = module.eks-cluster.eks_cluster_token
   cluster_ca_certificate = base64decode(module.eks-cluster.cluster_ca_certificate)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", "Project-Aurora-eks-cluster"]
+    command     = "aws"
+  }
 }
 
 terraform {
+  required_version = ">= 1.9.0"
+
   backend "s3" {
     key            = "terraform.tfstate"
     region         = "us-east-1"
@@ -20,5 +27,14 @@ terraform {
       source  = "hashicorp/tls"
       version = "~> 4.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.0"
+    }
   }
 }
+
